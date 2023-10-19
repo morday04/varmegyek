0d9fc9a522ea601b297b5f4b8b4c575b457e3462
3116TAN-18 <3116TAN-18@boronkay.local>
2023-10-19 13:17:33 +0200

    alter_table

---
+++ database/migrations/2023_09_21_111824_alter_table_clients.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    public function up(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->boolean('is_active')->default(true);
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->boolean('notify')->default(true);
+            $table->string('phone_number');
+            $table->string('adress')->nullable();
+            $table->boolean('is_company')->default(false);
+            $table->text('notes')->nullable();
+          
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function($table) {
+            $table->dropColumn('is_active');
+            $table->dropColumn('name');
+            $table->dropColumn('email');
+            $table->dropColumn('notify');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('adress');
+            $table->dropColumn('is_company');
+            $table->dropColumn('notes');
+        });
+    }
+};
