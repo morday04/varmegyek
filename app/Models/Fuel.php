d4398e31e1e94e1ae9daed0d9d3df692c173305e
Kovács László <laszlo.kovacs@trendency.hu>
2023-10-25 22:19:58 +0200

    add relation to models

--- app/Models/Fuel.php
+++ app/Models/Fuel.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Vehicle;
 
 class Fuel extends Model
 {
     use HasFactory;
+
+    public function vehicles()
+    {
+        return $this->hasMany(Vehicle::class);
+    }
 }
