d8529f0637ac385643b89485aad950cbf8330b73
Kovács László <laszlo.kovacs@trendency.hu>
2023-10-25 22:37:33 +0200

    update app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <!--<script type="module" src="{{ asset('js/app.js') }}"></script>-->
     <script src="{{ asset('build/assets/app-c75e0372.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    <!--<script src="{{ asset('js/workshop.js') }}"></script>-->
+    <script src="{{ asset('js/workshop.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
